3500654ad94bc106cce2f3128ea047c24a6f4088
Samuel Kamal <gergesk75@gmail.com>
2021-09-28 05:32:21 +0200

    Updated Settings

--- project/settings.py
+++ project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6rq4zpm-52*=d=wivv-u)q8#o(k&k8ucc3p#qu1pjcbc%e3v=^'
+SECRET_KEY = os.environ.get('saint_didymus_password')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saintdidymustheblind.herokuapp.com']
 
 
 # Application definition
