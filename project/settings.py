8c81793c2a575897f741988cd21a285eca8cdec0
Samuel Kamal <gergesk75@gmail.com>
2021-09-29 07:10:13 +0200

    securety key

--- project/settings.py
+++ project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('project\secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = 'a68acac1eac321715f6c5c85d898ea75029abc955be51e4f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
